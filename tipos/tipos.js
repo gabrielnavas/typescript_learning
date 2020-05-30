77eb5e2546aa776917309c214f4697ca062919a5
navas <gabrielnavas@email.com>
2020-05-30 12:35:38 -0300

    inserir tipo explicitamente nas variáveis

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -7,3 +7,13 @@ console.log(nome);
 var idade = 27;
 //idade = 'ana' => o tipo de idade é number
 idade = 27.5; //posso usar ponto flutuante
+// boolean
+var possuiHobbies = false;
+// possuiHobbies = 1; // possuiHobbies é boolean, number não pode ser atribuido
+// tipos explícitos
+var minhaIdade;
+console.log(typeof minhaIdade); // => any
+minhaIdade = 27; //tipo dinâmico
+console.log(typeof minhaIdade); // => number
+minhaIdade = 'gabriel';
+console.log(typeof minhaIdade); // => string
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,18 +1,29 @@
 
 //String
 
-let nome = "João"
+let nome:string = "João"
 console.log(nome)
 //nome = 28 => o tipo de nome é string
 
 
 
 //number
-let idade = 27;
+let idade:number = 27;
 //idade = 'ana' => o tipo de idade é number
 idade = 27.5 //posso usar ponto flutuante
 
 
 // boolean
-let possuiHobbies = false;
-possuiHobbies = 1; // possuiHobbies é boolean, number não pode ser atribuido
+let possuiHobbies: boolean = false;
+// possuiHobbies = 1; // possuiHobbies é boolean, number não pode ser atribuido
+
+
+// tipos explícitos
+let minhaIdade: any
+console.log(typeof minhaIdade) // => any
+
+minhaIdade = 27 //tipo dinâmico
+console.log(typeof minhaIdade) // => number
+
+minhaIdade = 'gabriel'
+console.log(typeof minhaIdade) // => string
