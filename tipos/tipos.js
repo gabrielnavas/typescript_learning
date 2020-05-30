7dae7f4bb52f830d5d8ba477254dca5fc6f9e38f
navas <gabrielnavas@email.com>
2020-05-30 12:22:37 -0300

    tipo string

---
+++ tipos/tipos.js
@@ -0,0 +1,5 @@
+"use strict";
+//String
+var nome = "João";
+console.log(nome);
+//nome = 28 => o tipo de nome é string
---
+++ tipos/tipos.ts
@@ -0,0 +1,6 @@
+
+//String
+
+let nome = "João"
+console.log(nome)
+//nome = 28 => o tipo de nome é string
