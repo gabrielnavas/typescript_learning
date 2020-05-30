fb70e17f97eace1572701ee6d32e67888c056f87
navas <gabrielnavas@email.com>
2020-05-30 12:24:00 -0300

    tipo number

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -3,3 +3,7 @@
 var nome = "João";
 console.log(nome);
 //nome = 28 => o tipo de nome é string
+//number
+var idade = 27;
+//idade = 'ana' => o tipo de idade é number
+idade = 27.5; //posso usar ponto flutuante
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -4,3 +4,12 @@
 let nome = "João"
 console.log(nome)
 //nome = 28 => o tipo de nome é string
+
+
+
+//number
+let idade = 27;
+//idade = 'ana' => o tipo de idade é number
+idade = 27.5 //posso usar ponto flutuante
+
+
