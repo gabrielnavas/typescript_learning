f205089dd85061f1ab907d9a1d9d1d5925645241
navas <gabrielnavas@email.com>
2020-06-04 18:39:46 -0300

    let & const 1

---
+++ ecmascript/ecmascript.js
@@ -0,0 +1,13 @@
+"use strict";
+// let & const
+//exemplo 1
+var seraQuePode;
+console.log(seraQuePode);
+seraQuePode = '?';
+//exemplo 2
+var estaFrio = true;
+if (estaFrio) {
+    var acao = 'Colocar o casaco!';
+}
+// console.log(acao); //compila
+//# sourceMappingURL=ecmascript.js.map
---
+++ ecmascript/ecmascript.ts
@@ -0,0 +1,14 @@
+// let & const
+
+//exemplo 1
+var seraQuePode;
+console.log(seraQuePode);
+seraQuePode = '?';
+
+
+//exemplo 2
+let estaFrio = true
+if(estaFrio) {
+    let acao = 'Colocar o casaco!';
+}
+// console.log(acao); //compila
