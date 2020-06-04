ce0a9aa77709bed0f313e8a0a1e64932fe7a07b6
navas <gabrielnavas@email.com>
2020-06-04 19:06:37 -0300

    this, analise lexica

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -1,5 +1,6 @@
 "use strict";
 // let & const
+var _this = this;
 //exemplo 1
 var seraQuePode;
 console.log(seraQuePode);
@@ -10,4 +11,31 @@ if (estaFrio) {
     var acao = 'Colocar o casaco!';
 }
 // console.log(acao); //compila
+// Arrow Function
+var somar = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(somar(2, 2));
+var subtrair = function (n1, n2) { return n1 - n2; };
+console.log(subtrair(4, 1));
+var saudacao = function () { return console.log('Olá'); };
+saudacao();
+var falarCom = function (pessoa) { return console.log('Olá ' + pessoa); };
+falarCom('João');
+//this, funcao sem arrow
+function normalComThis() {
+    console.log(this);
+}
+/*  o this na função normalComThis tera o valor do objeto,
+    e retorna para a variavel normalComThisEspecial
+    aqui o this varia
+*/
+var normalComThisEspecial = normalComThis.bind({ nome: 'gabriel' });
+normalComThisEspecial();
+// this, funcao com arrow
+/* na função arrowComThis, o this é sempre o mesmo.
+    aqui o this é sempre o mesmo do contexto no qual foi escrito.
+*/
+var arrowComThis = function () { return console.log(_this); };
+arrowComThis();
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -12,3 +12,42 @@ if(estaFrio) {
     let acao = 'Colocar o casaco!';
 }
 // console.log(acao); //compila
+
+
+// Arrow Function
+const somar = function(n1: number,n2: number): number {
+    return n1+n2;
+}
+console.log(somar(2,2))
+
+const subtrair = (n1: number, n2:number): number => n1 - n2;
+console.log(subtrair(4,1));
+
+
+const saudacao = () => console.log('Olá');
+saudacao();
+
+const falarCom = (pessoa: string) => console.log('Olá ' + pessoa);
+falarCom('João')
+
+
+//this, funcao sem arrow
+function normalComThis(){
+    console.log(this);
+}
+
+/*  o this na função normalComThis tera o valor do objeto, 
+    e retorna para a variavel normalComThisEspecial
+    aqui o this varia
+*/
+const normalComThisEspecial = normalComThis.bind({nome: 'gabriel'});
+normalComThisEspecial();
+
+
+// this, funcao com arrow
+
+/* na função arrowComThis, o this é sempre o mesmo.
+    aqui o this é sempre o mesmo do contexto no qual foi escrito.
+*/
+const arrowComThis = () => console.log(this);
+arrowComThis();
