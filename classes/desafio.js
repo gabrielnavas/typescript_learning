46c2e94d9c767c960cf2e24109acd958663a28eb
Navas <gabrielnavas@email.com>
2020-07-13 19:21:51 -0300

    desafio feito

--- classes/desafio.js
+++ classes/desafio.js
@@ -1,51 +1,52 @@
 "use strict";
 // Exercício 1 - Classe
-function Moto(nome) {
-    this.nome = nome;
-    this.velocidade = 0;
-    this.buzinar = function () {
-        console.log('Toooooooooot!');
-    };
-    this.acelerar = function (delta) {
+class Moto {
+    constructor(nome = '') {
+        this.nome = nome;
+        this.velocidade = 0;
+    }
+    buzinar() {
+        console.log('Toooooooooooot!');
+    }
+    acelerar(delta) {
         this.velocidade = this.velocidade + delta;
-    };
+    }
 }
-var moto = new Moto('Ducati');
+const moto = new Moto('Ducati');
 moto.buzinar();
 console.log(moto.velocidade);
 moto.acelerar(30);
 console.log(moto.velocidade);
 // Exercício 2 - Herança
-var objeto2D = {
-    base: 0,
-    altura: 0
-};
-var retangulo = Object.create(objeto2D);
-retangulo.base = 5;
-retangulo.altura = 7;
-retangulo.area = function () {
-    return this.base * this.altura;
-};
+class Objecto2D {
+    constructor(base = 0, altura = 0) {
+        this.base = base;
+        this.altura = altura;
+    }
+}
+class Retangulo extends Objecto2D {
+    constructor(base = 0, altura = 0) {
+        super(base, altura);
+    }
+    area() {
+        return this.base * this.altura;
+    }
+}
+const retangulo = new Retangulo(5, 7);
 console.log(retangulo.area());
 // Exercício 3 - Getters & Setters
-var estagiario = {
-    _primeiroNome: ''
-};
-Object.defineProperty(estagiario, 'primeiroNome', {
-    get: function () {
+class Estagiario {
+    constructor(_primeiroNome = '') {
+        this._primeiroNome = _primeiroNome;
+    }
+    get primeiroNome() {
         return this._primeiroNome;
-    },
-    set: function (valor) {
-        if (valor.length >= 3) {
-            this._primeiroNome = valor;
-        }
-        else {
-            this._primeiroNome = '';
-        }
-    },
-    enumerable: true,
-    configurable: true
-});
+    }
+    set primeiroNome(primeiroNome) {
+        primeiroNome.length >= 3 ? this._primeiroNome = primeiroNome : '';
+    }
+}
+const estagiario = new Estagiario('Gab');
 console.log(estagiario.primeiroNome);
 estagiario.primeiroNome = 'Le';
 console.log(estagiario.primeiroNome);
--- classes/desafio.ts
+++ classes/desafio.ts
@@ -1,59 +1,85 @@
-    // Exercício 1 - Classe
-    function Moto(nome) {
-        this.nome = nome
-        this.velocidade = 0
-     
-        this.buzinar = function() {
-            console.log('Toooooooooot!')
-        }
-     
-        this.acelerar= function(delta) {
-            this.velocidade = this.velocidade + delta
-        }
+// Exercício 1 - Classe
+class Moto {
+
+    public velocidade: number;
+
+    constructor(
+        public nome: string = '',
+    ) {
+        this.velocidade = 0;
     }
-     
-    var moto = new Moto('Ducati')
-    moto.buzinar()
-    console.log(moto.velocidade)
-    moto.acelerar(30)
-    console.log(moto.velocidade)
-     
-    // Exercício 2 - Herança
-    var objeto2D = {
-        base: 0,
-        altura: 0
+
+    buzinar(): void {
+        console.log('Toooooooooooot!');
     }
-     
-    var retangulo = Object.create(objeto2D)
-    retangulo.base = 5
-    retangulo.altura = 7
-    retangulo.area = function() {
-        return this.base * this.altura
+
+    acelerar(delta: number): void {
+        this.velocidade = this.velocidade + delta;
     }
-    console.log(retangulo.area())
-     
-    // Exercício 3 - Getters & Setters
-    var estagiario = {
-        _primeiroNome: ''
+}
+
+const moto = new Moto('Ducati');
+moto.buzinar();
+console.log(moto.velocidade)
+moto.acelerar(30);
+console.log(moto.velocidade)
+
+
+
+
+// Exercício 2 - Herança
+abstract class Objecto2D {
+
+    constructor(
+        public base: number = 0,
+        public altura: number = 0
+    ) { }
+
+    abstract area(): number;
+}
+
+class Retangulo extends Objecto2D {
+
+    constructor(
+        base: number = 0,
+        altura: number = 0,
+    ) {
+        super(base, altura);
     }
-     
-    Object.defineProperty(estagiario, 'primeiroNome', {
-        get: function () {
-            return this._primeiroNome
-        },
-        set: function (valor) {
-            if (valor.length >= 3) {
-                this._primeiroNome = valor
-            } else {
-                this._primeiroNome = ''
-            }
-        },
-        enumerable: true,
-        configurable: true
-    })
-     
-    console.log(estagiario.primeiroNome)
-    estagiario.primeiroNome = 'Le'
-    console.log(estagiario.primeiroNome)
-    estagiario.primeiroNome = 'Leonardo'
-    console.log(estagiario.primeiroNome)
+
+    public area(): number {
+        return this.base * this.altura;
+    }
+}
+
+const retangulo: Objecto2D = new Retangulo(5, 7);
+console.log((retangulo as Retangulo).area())
+
+
+
+
+
+// Exercício 3 - Getters & Setters
+
+class Estagiario {
+
+    constructor(
+        public _primeiroNome: string = '',
+    ) { }
+
+    get primeiroNome(): string {
+        return this._primeiroNome
+    }
+
+    set primeiroNome(primeiroNome: string) {
+        primeiroNome.length >= 3 ? this._primeiroNome = primeiroNome : '';
+    }
+}
+
+const estagiario = new Estagiario('Gab');
+
+console.log(estagiario.primeiroNome)
+estagiario.primeiroNome = 'Le'
+console.log(estagiario.primeiroNome)
+estagiario.primeiroNome = 'Leonardo'
+console.log(estagiario.primeiroNome)
