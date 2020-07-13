544cc1001a25edc1b528ff8af01b5d59abcd65d4
Navas <gabrielnavas@email.com>
2020-07-13 19:59:26 -0300

    namespaces aninhados

--- namespace/namespaces.js
+++ namespace/namespaces.js
@@ -1,16 +1,19 @@
 "use strict";
-var Areas;
-(function (Areas) {
-    const PI = 3.14;
-    function circuferencia(raio) {
-        return PI * Math.pow(raio, 2);
-    }
-    Areas.circuferencia = circuferencia;
-    function areaRetangulo(base, altura) {
-        return base * altura;
-    }
-    Areas.areaRetangulo = areaRetangulo;
-})(Areas || (Areas = {}));
-console.log(Areas.circuferencia(10));
-console.log(Areas.areaRetangulo(10, 20));
+var Geometria;
+(function (Geometria) {
+    let Area;
+    (function (Area) {
+        const PI = 3.14;
+        function circuferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.circuferencia = circuferencia;
+        function areaRetangulo(base, altura) {
+            return base * altura;
+        }
+        Area.areaRetangulo = areaRetangulo;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+console.log(Geometria.Area.circuferencia(10));
+console.log(Geometria.Area.areaRetangulo(10, 20));
 //# sourceMappingURL=namespaces.js.map
--- namespace/namespaces.ts
+++ namespace/namespaces.ts
@@ -1,17 +1,18 @@
+namespace Geometria {
+    export namespace Area {
+        const PI = 3.14;
+        
+        export function circuferencia(raio: number): number{
+            return PI * Math.pow(raio, 2);
+        }
+        
+        export function areaRetangulo(base: number, altura: number): number {
+            return base * altura;
+        }
 
-namespace Areas {
-    const PI = 3.14;
-    
-    export function circuferencia(raio: number): number{
-        return PI * Math.pow(raio, 2);
     }
-    
-    export function areaRetangulo(base: number, altura: number): number {
-        return base * altura;
-    }
-
 }
 
 
-console.log(Areas.circuferencia(10));
-console.log(Areas.areaRetangulo(10, 20));
+console.log(Geometria.Area.circuferencia(10));
+console.log(Geometria.Area.areaRetangulo(10, 20));
