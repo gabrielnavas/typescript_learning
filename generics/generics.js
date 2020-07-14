eec79282faef1de036cfcc1358944fc607c4d776
Navas <gabrielnavas@email.com>
2020-07-14 17:53:42 -0300

    generics with Array

--- generics/generics.js
+++ generics/generics.js
@@ -15,4 +15,9 @@ function echoMelhorado2(objeto) {
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado2(27).toString());
 console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
+// Generics disponíveis na API
+const avaliacoes = [10, 9.3, 7.7];
+// avaliacoes.push('43');
+avaliacoes.push(43);
+console.log(avaliacoes);
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -20,3 +20,8 @@ console.log(echoMelhorado<string>('João').length);
 console.log(echoMelhorado2(27).toString());
 console.log(echoMelhorado<{nome: string, idade: number}>({nome: 'João', idade: 27}).nome);
 
+// Generics disponíveis na API
+const avaliacoes: Array<number> = [10, 9.3, 7.7];
+// avaliacoes.push('43');
+avaliacoes.push(43);
+console.log(avaliacoes);
