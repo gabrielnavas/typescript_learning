49f1722b0216ae87d3ee2961d411638af5575c71
Navas <gabrielnavas@email.com>
2020-07-14 15:22:01 -0300

    usando interfaces com classes

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -10,7 +10,25 @@ function mudarNome(pessoa) {
 const pessoa = {
     nome: 'João',
     idade: 27,
+    saudar(sobrenome) {
+        console.log(`Olá meu nome é Mr ${sobrenome}, ${this.nome}`);
+    }
 };
 // saudarComOla(pessoa);
-saudarComOla({ nome: 'gabriel', idade: 27, obs: 'sou um attr dinâmico' });
+// saudarComOla({nome: 'gabriel', idade: 27, obs: 'sou um attr dinâmico'});
+pessoa.saudar('Navas');
+// usando classes
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date();
+    }
+    saudar(sobrenome) {
+        console.log(`Olá meu nome é ${this.nome} ${sobrenome}`);
+    }
+}
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -2,6 +2,7 @@ interface Humano {
     nome: string,
     idade? : number,
     [prop: string]: any,
+    saudar(sobrenome: string): void;
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -17,6 +18,28 @@ function mudarNome(pessoa: Humano) {
 const pessoa: Humano = {
     nome: 'João',
     idade: 27,
+    saudar(sobrenome: string): void {
+        console.log(`Olá meu nome é Mr ${sobrenome}, ${this.nome}`)
+    }
 }
 // saudarComOla(pessoa);
-saudarComOla({nome: 'gabriel', idade: 27, obs: 'sou um attr dinâmico'});
+// saudarComOla({nome: 'gabriel', idade: 27, obs: 'sou um attr dinâmico'});
+
+pessoa.saudar('Navas');
+
+
+// usando classes
+
+
+class Cliente implements Humano {
+    nome: string = '';
+    ultimaCompra: Date = new Date();
+    saudar(sobrenome: string) {
+        console.log(`Olá meu nome é ${this.nome} ${sobrenome}`);
+    }
+}
+
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
