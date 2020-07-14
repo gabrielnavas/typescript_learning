f09b0605f2e3afc53b04b9372dbe048581534a12
Navas <gabrielnavas@email.com>
2020-07-14 15:03:40 -0300

    interfaces e propriedades dinâmicos

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -1,6 +1,8 @@
 "use strict";
 function saudarComOla(pessoa) {
     console.log('Olá, ' + pessoa.nome);
+    console.log(`tenho ${pessoa.idade} anos`);
+    console.log(`e tenho uma OBS: ${pessoa.obs}.`);
 }
 function mudarNome(pessoa) {
     pessoa.nome = 'Joana';
@@ -9,5 +11,6 @@ const pessoa = {
     nome: 'João',
     idade: 27,
 };
-saudarComOla(pessoa);
+// saudarComOla(pessoa);
+saudarComOla({ nome: 'gabriel', idade: 27, obs: 'sou um attr dinâmico' });
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,18 +1,22 @@
 interface Humano {
     nome: string,
+    idade? : number,
+    [prop: string]: any,
 }
 
 function saudarComOla(pessoa: Humano) {
     console.log('Olá, ' + pessoa.nome);
+    console.log(`tenho ${pessoa.idade} anos`);
+    console.log(`e tenho uma OBS: ${pessoa.obs}.`);
 }
 
 function mudarNome(pessoa: Humano) {
     pessoa.nome = 'Joana';
 }
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'João',
     idade: 27,
 }
-
-saudarComOla(pessoa);
+// saudarComOla(pessoa);
+saudarComOla({nome: 'gabriel', idade: 27, obs: 'sou um attr dinâmico'});
