21ad9cfaada4186a4f955786828fb8e8806f68b3
Navas <gabrielnavas@email.com>
2020-07-14 16:28:43 -0300

    generics with functions

--- generics/generics.js
+++ generics/generics.js
@@ -2,7 +2,17 @@
 function echo(objeto) {
     return objeto;
 }
-console.log(echo('João').length);
-console.log(echo(27).length);
-console.log(echo({ nome: 'João', idade: 27 }));
+// console.log(echo('João').length);
+// console.log(echo(27).length);
+// console.log(echo({nome: 'João', idade: 27}));
+// Usando Generics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+function echoMelhorado2(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado('João').length);
+console.log(echoMelhorado2(27).toString());
+console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -1,6 +1,22 @@
 function echo(objeto: any) {
     return objeto;
 }
-console.log(echo('João').length);
-console.log(echo(27).length);
-console.log(echo({nome: 'João', idade: 27}));
+// console.log(echo('João').length);
+// console.log(echo(27).length);
+// console.log(echo({nome: 'João', idade: 27}));
+
+
+// Usando Generics
+function echoMelhorado<T>(objeto: T): T {
+    return objeto;
+}
+
+function echoMelhorado2<TIPO>(objeto: TIPO): TIPO {
+    return objeto;
+}
+
+
+console.log(echoMelhorado<string>('João').length);
+console.log(echoMelhorado2(27).toString());
+console.log(echoMelhorado<{nome: string, idade: number}>({nome: 'João', idade: 27}).nome);
+
