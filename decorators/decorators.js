fb40fda28706b10f0790ab594f109649a1a6e791
Navas <gabrielnavas@email.com>
2020-07-15 08:24:37 -0300

    decorator factory

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,18 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+// @logarClass
+// @logarClasseSe(false)
 let Eletrodomestico = class Eletrodomestico {
     constructor() {
         console.log('novo....');
     }
 };
 Eletrodomestico = __decorate([
-    logarClass
+    factoryDecorator({ a: 'Olá ', b: 2 })
 ], Eletrodomestico);
 function logarClass(construtor) {
-    console.log(construtor);
+    console.log(typeof construtor);
 }
-new Eletrodomestico();
-new Eletrodomestico();
-new Eletrodomestico();
+function decoratorVazio(_) {
+}
+function logarClasseSe(valor) {
+    return valor ? logarClass : decoratorVazio;
+}
+function factoryDecorator(obj) {
+    return function (construtor) {
+        console.log(obj.a + ' ' + obj.b);
+    };
+}
 //# sourceMappingURL=decorators.js.map
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,15 +1,26 @@
-@logarClass
+// @logarClass
+// @logarClasseSe(false)
+@factoryDecorator({ a: 'Olá ', b: 2 })
 class Eletrodomestico {
- 
+
     constructor() {
         console.log('novo....')
     }
 }
 
 function logarClass(construtor: Function) {
-    console.log(construtor);
+    console.log(typeof construtor);
 }
 
-new Eletrodomestico();
-new Eletrodomestico();
-new Eletrodomestico();
+function decoratorVazio(_: Function) {
+}
+
+function logarClasseSe(valor: boolean) {
+    return valor ? logarClass : decoratorVazio;
+}
+
+function factoryDecorator(obj: { a: string, b: number }) {
+    return function (construtor: Function): void {
+        console.log(obj.a + ' ' + obj.b);
+    }
+}
