686d52f7486fc37ec6aebe2139ba2590a7de22ea
Navas <gabrielnavas@email.com>
2020-07-15 21:58:40 -0300

    add biblioteca no projeto

--- typescript_learning_features/integracao_gulp/src/main.ts
+++ typescript_learning_features/integracao_gulp/src/main.ts
@@ -1,5 +1,9 @@
-import Livro from './modelo/livro'
+import $ from 'jquery';
+import Livro from './modelo/livro';
 
 
 const livro = new Livro('Dom Quixote', 108.80, 0.10);
 console.log(livro.precoComDesconto());
+
+$('body').append(`<h1>${livro.nome}</h1>`)
+$('body').append(`<h2>${livro.precoComDesconto()}</h12`)
